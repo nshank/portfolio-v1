c0751d79c469416674c25fa8efe04507dcf7f6ae
Nick <nicholas.shank@gmail.com>
2014-02-27 22:15:43 -0600

    added dist directory

--- dist/scripts/44466bee.scripts.js
+++
@@ -1,1 +0,0 @@
-"use strict";angular.module("portfolioApp",["ngCookies","ngResource","ngSanitize","ngRoute"]).config(["$routeProvider",function(a){a.when("/",{templateUrl:"views/main.html",controller:"MainCtrl"}).otherwise({redirectTo:"/"})}]),angular.module("portfolioApp").controller("MainCtrl",["$scope",function(a){a.awesomeThings=["HTML5 Boilerplate","AngularJS","Karma"]}]);
