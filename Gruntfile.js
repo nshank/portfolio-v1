a3bb1a3b9eca0159c4a8056cb3241e6b978d24b2
Nick <nicholas.shank@gmail.com>
2014-02-27 22:32:42 -0600

    update gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -244,7 +244,8 @@ module.exports = function (grunt) {
         files: [{
           expand: true,
           cwd: '<%= yeoman.dist %>',
-          src: ['*.html', 'views/{,*/}*.html'],
+          // src: ['*.html', 'views/{,*/}*.html'],
+          src: ['*.html', 'views/**/*.html'],
           dest: '<%= yeoman.dist %>'
         }]
       }
